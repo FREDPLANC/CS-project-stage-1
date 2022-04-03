d28db448ed96178d5e450e26f99350c5360ee8d1
StriderWHY <75156601+StriderWYH@users.noreply.github.com>
2022-04-03 20:47:30 +0800

    Add files via upload
    处理预约和撤销预约操作

---
+++ process.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include "localq.h"
+#include "centerq.h"
+using namespace std;
+//处理预约和撤销操作
+template<class T> void  centerHeap<T>::appointment_process(int date )
+{
+    pop_patient_wrtddl(min,date);
+    for (int i = 0; i < capacitor_total;i++){
+        min->treated_time = ddl + 1;
+        min->treated_location = fine_nearest_hospital();
+        removeMin();
+    }
+    return;
+}
+
+template<class T> void  centerHeap<T>::withdraw(int id)
+{
+    centerNode<T>* p = search_id(id);
+    remove(p);
+    withdraw_list[id] = 1;
+}
